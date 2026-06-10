3bc0cae342be19a27a0ad0581002dc2a129acbbb
kevin12369 <491750329@qq.com>
2026-06-10 10:26:11 +0800

    feat(sandbox): expose public API

---
+++ packages/sandbox/src/index.ts
@@ -0,0 +1,11 @@
+export { extractHtml } from './extractHtml';
+export { sizeCheck, MAX_BYTES } from './sizeCheck';
+export { staticAnalysis } from './staticAnalysis';
+export { FORBIDDEN_PATTERNS, findDenylistHit } from './denylist';
+export {
+  PROTOCOL_VERSION,
+  ALLOWED_MESSAGE_TYPES,
+  isAllowedMessage,
+} from './protocol';
+export type { GameMessage, GameMessageType, ValidationResult, DenylistHit } from './types';
+export type { ValidationResult as SandboxValidation } from './types';
